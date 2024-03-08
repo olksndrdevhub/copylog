a3627a75acf3a9c5e2700b90a11236aa8d202e60
Oleksandr Romaniuk <oleksandr.romaniuk@protonmail.com>
2024-03-08 20:11:40 +0200

    add air

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/olksndrdevhub/copylog
 
 go 1.22.0
 
-require fyne.io/fyne/v2 v2.4.4
+require (
+	fyne.io/fyne/v2 v2.4.4
+	golang.design/x/clipboard v0.7.0
+)
 
 require (
 	fyne.io/systray v1.10.1-0.20231115130155-104f5ef7839e // indirect
@@ -25,6 +28,7 @@ require (
 	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/tevino/abool v1.2.0 // indirect
 	github.com/yuin/goldmark v1.5.5 // indirect
+	golang.org/x/exp v0.0.0-20200224162631-6cc2880d07d6 // indirect
 	golang.org/x/image v0.11.0 // indirect
 	golang.org/x/mobile v0.0.0-20230531173138-3c911d8e3eda // indirect
 	golang.org/x/net v0.17.0 // indirect
--- main.go
+++ main.go
@@ -1,23 +1,51 @@
 package main
 
 import (
+	"context"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	"golang.design/x/clipboard"
 )
 
+func ReadClipboard() {
+	clipboard_channel := clipboard.Watch(context.TODO(), clipboard.FmtText)
+	for item := range clipboard_channel {
+		//  print clipboard content
+		println("New item in clipboard: ", string(item))
+	}
+}
+
 func main() {
+	err := clipboard.Init()
+	if err != nil {
+		panic(err)
+	}
 	app := app.New()
 	window := app.NewWindow("CopyLog")
 
-	hello := widget.NewLabel("Hi CopyLog!")
-	window.SetContent(container.NewVBox(
+	hello := widget.NewLabel("Welcome to  CopyLog!")
+
+	name_input := widget.NewEntry()
+  name_input.SetPlaceHolder("Enter your name...")
+	name_input.Resize(fyne.NewSize(200, 30))
+	name_input.Move(fyne.NewPos(0, 60))
+
+	hello_btn := widget.NewButton("Submit", func() {
+		println("Hello ", name_input.Text)
+		hello.SetText("Hello " + name_input.Text + "!")
+	})
+	hello_btn.Resize(fyne.NewSize(100, 20))
+	hello_btn.Move(fyne.NewPos(0, 120))
+
+	window.SetContent(container.NewWithoutLayout(
 		hello,
-		widget.NewButton("Hello", func() {
-			hello.SetText("Welcome :)")
-		}),
+		name_input,
+		hello_btn,
 	))
-
+	window.Resize(fyne.NewSize(600, 300))
 	window.ShowAndRun()
-
+	ReadClipboard()
 }
